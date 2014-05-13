e4cd8a75fa8dc0424c435c6dcc517407e493e93d
Rohan Mathur <rmathur101@gmail.com>
2014-05-13 10:28:55 -0500

    Added more stuff

--- pizza/lib/spec/pizz_spec.rb
+++ pizza/lib/spec/pizz_spec.rb
@@ -6,4 +6,8 @@ describe "Pizza model" do
   it "should have attribute name that is initalized to 'name'"  do
     expect(pizza.name).to eq("name")
   end
+
+  it "should have two other attributes description and time_baked not intitialized"
+    expect(pizza.description).to be_nil
+    expect(pizza.time_baked).to be_nil
 end
