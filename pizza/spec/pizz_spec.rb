c366e3c0f015a8ed62d79a950de192621cbd5601
Rohan Mathur <rmathur101@gmail.com>
2014-05-13 11:23:12 -0500

    new tests

--- pizza/spec/pizz_spec.rb
+++ pizza/spec/pizz_spec.rb
@@ -7,9 +7,21 @@ describe "Pizza model" do
   it "should have attribute name that is initalized to 'name'"  do
     expect(pizza.name).to eq("name")
   end
+  it "should have attribute time_baked initialzed to 0" do
+    expect(pizza.time_baked).to eq(0)
+  end
 
   it "should have two other attributes description and time_baked not intitialized" do
-    expect(pizza.description).to be_nil
-    expect(pizza.time_baked).to be_nil
+    expect(pizza).to respond_to(:description)
   end
+
+  it "should have many toppings (data structure is array)"  do
+    expect(pizza).to respond_to(:toppings)
+  end
+
+  it "has method: #required_bake_time" do
+    expect(pizza).to respond_to(:required_bake_time)
+  end
+
+
 end
